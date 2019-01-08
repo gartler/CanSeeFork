a0c23c46734fe68ebaa2dae97d50e1173a31643c
Notice B.V <jmeijer@notice.nu>
2019-01-08 18:07:14 +0100

    Support for 2 CANbuses added CANbus handling separated out for readablility

---
+++ include/canhandler.h
@@ -0,0 +1,16 @@
+#ifndef _CANHANDLER_H_
+#define _CANHANDLER_H_
+
+#include <Arduino.h>
+
+#include "config.h"
+
+// Repository included libraries includes, see ./lib/ ************************
+#include "ESP32CAN.h"
+#include "CAN_config.h"
+
+void can_init (CS_CONFIG *config);
+void can_send (CAN_frame_t *frame, uint8_t bus);
+void can_send_flow (uint16_t requestId);
+
+#endif
