aae51d65508886368b6a801fb13e2e4ce1022001
Notice B.V <jmeijer@notice.nu>
2019-01-12 22:54:59 +0100

    Error fix in BT (in)active code

--- src/bluetoothhandler.cpp
+++ src/bluetoothhandler.cpp
@@ -15,6 +15,10 @@ void bluetooth_init () {
 }
 
 void setActiveBluetooth (bool active) {
+  // If active is false in and output is simply ignored. At the moment This
+  // is a safer solution than switching off Bluetooth or BluetoothSerial as
+  // there seems to be a nasty memory leak in that part of the core, see
+  // https://github.com/espressif/arduino-esp32/issues/1798
   if (!bluetooth_config->mode_bluetooth) return;
   bluetooth_active = active;
 }
@@ -24,13 +28,13 @@ void writeOutgoingBluetooth (String o) {
 }
 
 void readIncomingBluetooth (String &readBuffer) {
-  if (bluetooth_active) return;
+  if (!bluetooth_config->mode_bluetooth) return;
   led_set (LED_BLUE, SerialBT.hasClient());
   if (!SerialBT.available()) return;
   char ch = SerialBT.read();
   if (ch == '\n' || ch == '\r') {
     if (readBuffer != "") {
-      if (bluetooth_config->command_handler) bluetooth_config->command_handler ();
+      if (bluetooth_active && bluetooth_config->command_handler) bluetooth_config->command_handler ();
       readBuffer = "";
     }
   } else {
