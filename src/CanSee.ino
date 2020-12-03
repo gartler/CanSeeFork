975cded748aecdfb99cc567a794194e13bcf60bf
Notice B.V <jmeijer@notice.nu>
2020-12-03 22:40:18 +0100

    Bumped version

--- src/CanSee.ino
+++ src/CanSee.ino
@@ -19,7 +19,7 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-#define VERSION "005"
+#define VERSION "006"
 
 #define SERIAL_BPS 115200
 
